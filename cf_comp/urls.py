33629a177193aa592529a6a238ac7b87ab4e255d
the-dg04 <devgup04@gmail.com>
2023-11-06 01:48:20 +0530

    gfdsg

--- cf_comp/urls.py
+++ cf_comp/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path("",views.home),
     path("register/",view1.register),
     path("login/",view1.login),
-    path('user/<str:username>',views.userPage),
-    path('user/<str:username>/submissions',views.userSubmissions)
+    path('user/',views.userRedirect),
+    path('user/<str:username>/submissions',views.userSubmissions),
+    path('logout/',views.logout)
 ]
--- cf_comp/views.py
+++ cf_comp/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from cf_comp.api_calls.api_func import status
 
+def isLoggedIn(request):
+    try:
+        username=request.COOKIES['username']
+        return username
+    except:
+        return False
+
+def getLoginStatus(request):
+    loginStatus=False
+    if(isLoggedIn(request)):
+        loginStatus=True
+    return loginStatus
+
 def home(request):
-    return render(request,"getStarted.html")
+    return render(request,"getStarted.html",{'isLoggedIn':getLoginStatus(request)})
 
 def userPage(request,username):
     return HttpResponse(f'hello : {username}')
 
 # def getRatingsFromSubmissions()
+def userRedirect(request):
+    usernameOrNull=isLoggedIn(request)
+    if(usernameOrNull):
+        return redirect(f'/user/{usernameOrNull}/submissions')
+    else:
+        return redirect('/login')
 
 def userSubmissions(request,username):
+    if(not isLoggedIn(request)):
+       return redirect('/login')
     sub = status(username)
     ratings=[]
     for i in sub:
@@ -31,3 +52,9 @@ def userSubmissions(request,username):
         'y':list(rating_dict.values())
     }
     return render(request,"submissions.html",{'sub':sub[:10],'data':data})
+
+def logout(request):
+    response=redirect('/')
+    response.delete_cookie('username')
+    return response
+
