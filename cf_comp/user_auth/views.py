d40c9cf63887b19427ad9f108120396e5cc077f9
the-dg04 <devgup04@gmail.com>
2023-11-05 19:18:58 +0530

    sdfvgedfsg

--- cf_comp/user_auth/views.py
+++ cf_comp/user_auth/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+import requests
+
+def isValid(username):
+    response=requests.get(f"https://codeforces.com/api/user.info?handles={username}")
+    resp=response.json()
+    return resp['status']=='OK'
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('/')
+            if(isValid(username)):
+                messages.success(request, f'Account created for {username}!')
+                return redirect('/')
+            else:
+                messages.warning(request,"Invalid CF handle")
+                # return render(request,"register.html",{'form':form})
     else:
         form = UserCreationForm()
     return render(request,"register.html",{'form':form})
