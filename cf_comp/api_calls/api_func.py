62f7967f1fdb4fe1e341b980273dd1b6dd5f040f
Nikhil Totla <nikhil.totla.cse22@itbhu.ac.in>
2023-11-06 04:04:59 +0530

    usd

--- cf_comp/api_calls/api_func.py
+++ cf_comp/api_calls/api_func.py
@@ -28,6 +28,27 @@ def rating(username):
         return user_info['result'][-1]['newRating']
     else:
         return None
+def problem(tag,rating):
+    # username = 'nikhiltotla'
+    url = 'https://codeforces.com/api/problemset.problems?'
+    response = requests.get(f'{url}tags={tag}')
+    if response.status_code == 200:
+        user_info =response.json()
+        b = []
+        for c1 in user_info['result']['problems']:
+            try:
+                c1['rating']
+            except:
+                continue
+            
+            if(c1['rating'] == rating):
+                b.append({'contest' : c1['contestId'],'index' : c1['index'], 'name' : c1['name']})
+        return b
+    else:
+        return None
 
 # print(rating('nikhiltotla')['result'][-1]['newRating'])
 # print(rating('nikhiltotla'))
+# b = []
+# print(problem('dp',1700)[:10])
+# print(b[:10])
