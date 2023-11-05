45e8584d1404295bebce993e867fb0fa9e239d3f
Nikhil Totla <nikhil.totla.cse22@itbhu.ac.in>
2023-11-05 21:30:13 +0530

    hehd

--- cf_comp/views.py
+++ cf_comp/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render,HttpResponse
 from cf_comp.api_calls.api_func import status
 def home(request):
     return render(request,"getStarted.html")
-
-def submissions(request):
-    username = 'ttirth1234518'
-    sub = status(username)
-    return render(request,"submissions.html",{'sub':sub})
-    # return HttpResponse('hi')
 def home(request):
     return render(request,"getStarted.html")
 
@@ -15,4 +9,5 @@ def userPage(request,username):
     return HttpResponse(f'hello : {username}')
 
 def userSubmissions(request,username):
-    return HttpResponse(f'submissions {username}')
+    sub = status(username)
+    return render(request,"submissions.html",{'sub':sub})
