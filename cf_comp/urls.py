e54999c618a901eeda9891122e435ab747fc4365
Nikhil Totla <nikhil.totla.cse22@itbhu.ac.in>
2023-11-05 17:18:29 +0530

    fck off

--- cf_comp/urls.py
+++ cf_comp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
+from .user_auth import views as view1
 
 urlpatterns=[
-    path("",views.home)
+    path("",views.home),
+    path("register/",view1.register)
 ]
